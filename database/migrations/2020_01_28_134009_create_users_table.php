bbd1deebcbe1d4b632f0bd8685269b630829c54c
Andrew <andrew@masterdef.net>
2020-01-28 22:24:00 +0000

    upd

---
+++ database/migrations/2020_01_28_134009_create_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('email',200)->unique();
+            $table->string('password',200);
+            $table->string('api_token',200);
+            $table->dateTime('token_expire_at');
+            $table->boolean('is_active')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
